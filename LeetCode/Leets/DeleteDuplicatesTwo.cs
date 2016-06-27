86b762c38a780bcbffe8fdb8ade1c45859ce9794
YouenZeng <raaaazer@gmail.com>
2016-06-28 00:57:36 +0800

    qwer

--- LeetCode/Leets/DeleteDuplicatesTwo.cs
+++ LeetCode/Leets/DeleteDuplicatesTwo.cs
@@ -16,44 +16,62 @@ namespace LeetCode.Leets
             fakedHead.next = head;
 
             ListNode prevHead = fakedHead;
-            ListNode currentNode = fakedHead.next;
+            ListNode prevDiff = fakedHead.next;
 
-            while (currentNode != null)
+            while (prevDiff != null)
             {
-                while (currentNode.next != null && currentNode.val == currentNode.next.val)
+                if (prevDiff.next != null && prevDiff.val == prevDiff.next.val)
                 {
-                    currentNode = currentNode.next;
+                    int currV = prevDiff.val;
+                    while (prevDiff.next != null)
+                    {
+                        prevDiff = prevDiff.next;
+                        if (prevDiff.next == null || prevDiff.next.val != currV)
+                        {
+                            break;
+                        }
+                    }
                 }
-                if (prevHead.next == currentNode)
+
+                if (prevHead.next == prevDiff)
                 {
                     prevHead = prevHead.next;
                 }
                 else
                 {
-                    prevHead.next = currentNode.next;
+                    prevHead.next = prevDiff.next;
                 }
-                currentNode = currentNode.next;
+                prevDiff = prevDiff.next;
+
+
+                //                else
+                //                {
+                //                    prevHead.next = prevDiff;
+                //                    prevHead = prevHead.next;
+                //                    
+                //                    prevDiff = prevDiff.next;
+                //                }
             }
             return fakedHead.next;
         }
         public void Execute()
         {
             ListNode node1 = new ListNode(1);
-            ListNode node2 = new ListNode(2);
-            ListNode node3 = new ListNode(3);
-//            ListNode node4 = new ListNode(2);
-//            ListNode node5 = new ListNode(3);
-//            ListNode node6 = new ListNode(4);
-//            ListNode node7 = new ListNode(4);
+            ListNode node2 = new ListNode(1);
+            ListNode node3 = new ListNode(1);
+            //            ListNode node4 = new ListNode(2);
+            //            ListNode node5 = new ListNode(3);
+            //            ListNode node6 = new ListNode(4);
+            //            ListNode node7 = new ListNode(4);
             node1.next = node2;
             node2.next = node3;
             node3.next = null;
-//            node2.next = node3;
-//            node3.next = node4;
-//            node4.next = node5;
-//            node5.next = node6;
-//            node6.next = node7;
-//            node7.next = null;
+            //            node2.next = node3;
+            //            node3.next = node4;
+            //            node4.next = node5;
+            //            node5.next = node6;
+            //            node6.next = node7;
+            //            node7.next = null;
             DeleteDuplicates(node1);
 
         }
