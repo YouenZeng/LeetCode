e66112a1087c4bcce5f200ade2bf1fe9c1a45a29
YouenZeng <raaaazer@gmail.com>
2016-11-30 15:13:29 +0800

    A not that easy problem

---
+++ LeetCode/LeetAgain/FindAnagramsSln.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Leets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetAgain
+{
+    class FindAnagramsSln : ISolution
+    {
+
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            throw new NotImplementedException();
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
