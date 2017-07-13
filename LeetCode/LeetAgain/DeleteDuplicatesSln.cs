3efa05184a8e3675f507c86bf30462a834cbd498
Bran Zeng <YouEn.Zeng2@adp.com>
2017-07-13 16:31:50 +0800

    3 linked list

---
+++ LeetCode/LeetAgain/DeleteDuplicatesSln.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Leets;
+
+namespace LeetCode.LeetAgain
+{
+    public class DeleteDuplicatesSln : ISolution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head?.next == null) return head;
+
+            ListNode fakeHead = head;
+
+            while (head.next != null)
+            {
+                if (head.val == head.next.val)
+                    head.next = head.next.next;
+                else
+                    head = head.next;
+            }
+            return fakeHead;
+        }
+        ListNode BuildNode(int[] arr)
+        {
+            ListNode header = new ListNode(arr[0]);
+            ListNode tempheader = header;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                ListNode tmp = new ListNode(arr[i]);
+                tempheader.next = tmp;
+                tempheader = tmp;
+            }
+            return header;
+        }
+
+        void ISolution.Execute()
+        {
+            ListNode head = BuildNode(new[] { 1, 1 });
+            DeleteDuplicates(head);
+        }
+    }
+}
---
+++ LeetCode/LeetAgain/ReverseBetweenSln.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Leets;
+
+namespace LeetCode.LeetAgain
+{
+    public class ReverseBetweenSln : ISolution
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            ListNode fakedHeader = new ListNode(123);
+            fakedHeader.next = head;
+
+            int currentIndex = 0;
+            ListNode currentHeader = fakedHeader;
+            ListNode prevHeader = fakedHeader;
+
+            while (currentIndex <= n)
+            {
+                if (currentIndex == m)
+                {
+                    ListNode tmpEnd = currentHeader;
+
+                    ListNode newHeader = null;
+                    while (currentIndex <= n)
+                    {
+                        currentIndex++;
+                        tmpEnd = tmpEnd.next;
+
+                        ListNode next = currentHeader.next;
+                        currentHeader.next = newHeader;
+                        newHeader = currentHeader;
+                        currentHeader = next;
+                    }
+
+                    prevHeader.next = newHeader;
+                    while (prevHeader.next != null)
+                    {
+                        prevHeader = prevHeader.next;
+                    }
+                    prevHeader.next = currentHeader;
+                }
+                else
+                {
+                    prevHeader = currentHeader;
+                    currentHeader = currentHeader.next;
+                }
+                currentIndex++;
+            }
+            return fakedHeader.next;
+        }
+        ListNode BuildNode(int[] arr)
+        {
+            ListNode header = new ListNode(arr[0]);
+            ListNode tempheader = header;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                ListNode tmp = new ListNode(arr[i]);
+                tempheader.next = tmp;
+                tempheader = tmp;
+            }
+            return header;
+        }
+
+        void ISolution.Execute()
+        {
+            ListNode head = BuildNode(new[] { 1, 2});
+            ReverseBetween(head, 1,2);
+        }
+    }
+}
