c64c291b2b0232f3ff07375418ac0170c6ab6b45
Bran Zeng <YouEn.Zeng2@adp.com>
2017-07-17 17:59:12 +0800

    dp

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
 
-            ISolution sln = new MinStack();
+            ISolution sln = new WordBreakSln();
             sln.Execute();
 
             Console.ReadLine();
