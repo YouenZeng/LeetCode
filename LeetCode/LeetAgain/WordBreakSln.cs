009d6fa6261a00604d340e9135794b89fb8de47f
Bran Zeng <YouEn.Zeng2@adp.com>
2017-10-09 11:09:01 +0800

    WIP

---
+++ LeetCode/LeetAgain/WordBreakSln.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetAgain
+{
+    class WordBreakSln : ISolution
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            int stringLength = s.Length;
+            bool[] dp = new bool[stringLength + 1];
+
+            dp[0] = true;
+
+            for (int i = 1; i <= stringLength; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordDict.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[stringLength];
+        }
+        public void Execute()
+        {
+            WordBreak("leetcodere", new[] { "leet", "co","d","e", "re"});
+        }
+    }
+}
