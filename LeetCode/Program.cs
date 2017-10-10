c4b0ea6f401496a7e91bc021fd3d7d645000b564
Bran Zeng <YouEn.Zeng2@adp.com>
2017-10-10 14:51:14 +0800

    2 more ak

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
 
-            ISolution sln = new KSmallestPairsSln();
+            ISolution sln = new ClimbStairsSln();
             sln.Execute();
 
             Console.ReadLine();
