7dbb8b77b36ae30e5b3a042fdcfd0b4d9a6a2bf7
YouenZeng <raaaazer@gmail.com>
2017-12-19 22:55:54 +0800

    2 two pointers solution

---
+++ LeetCode/LeetAgain/DetectCycleSln.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Leets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetAgain
+{
+    class DetectCycleSln : ISolution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            ListNode fast = head, slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow)
+                {
+                    ListNode slow2 = head;
+                    while (slow != slow2)
+                    {
+                        slow2 = slow2.next;
+                        slow = slow.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode/LeetAgain/HasCycleSln.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Leets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetAgain
+{
+    class HasCycleSln : ISolution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+            ListNode slowNode = head;
+            ListNode fastNode = head;
+
+            while (fastNode.next != null && fastNode.next.next != null)
+            {
+                slowNode = slowNode.next;
+                fastNode = fastNode.next.next;
+                if (slowNode == fastNode) return true;
+            }
+            return false;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
