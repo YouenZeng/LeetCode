515cee045452590f0d6cedfa393bd9a0b3637340
Bran Zeng <YouEn.Zeng2@adp.com>
2018-01-15 18:39:50 +0800

    WIP

---
+++ LeetCode/LeetAgain/BSTIteratorSln.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Leets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    /// <summary>
+    /// https://leetcode.com/problems/binary-search-tree-iterator/description/
+    /// 
+    /// </summary>
+    public class BSTIterator : ISolution
+    {
+
+        TreeNode currentRoot;
+        public BSTIterator(TreeNode root)
+        {
+            currentRoot = root;
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext()
+        {
+            return currentRoot.left != null;
+        }
+
+        /** @return the next smallest number */
+        public int Next()
+        {
+            if (HasNext())
+            {
+                BSTIterator inner = new BSTIterator(currentRoot.left);
+                return inner.Next();
+            }
+            return currentRoot.val;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
