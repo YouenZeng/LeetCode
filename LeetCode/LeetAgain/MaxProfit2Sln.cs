688d630ed9d30c5e62ffa8c45e50f77332e2779f
YouenZeng <raaaazer@gmail.com>
2018-02-28 00:07:59 +0800

    WIP

---
+++ LeetCode/LeetAgain/MaxProfit2Sln.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class MaxProfit2Sln : ISolution
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            int currentMin = int.MaxValue;
+            int currentProfit = 0;
+            int totalProfit = 0;
+            int currentMax = int.MaxValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] + fee < currentMax)
+                {
+                    currentMin = prices[i];
+                    totalProfit += currentProfit;
+                    currentMax = int.MinValue;
+                    currentProfit = 0;
+                }
+
+                if(currentProfit ==0 && prices[i] < currentMin)
+                {
+                    currentMin = prices[i];
+                }
+
+                currentMax = Math.Max(prices[i], currentMax);
+                currentProfit = Math.Max(prices[i] - currentMin - fee, currentProfit);
+            }
+
+            return totalProfit + currentProfit;
+
+        }
+        public void Execute()
+        {
+            MaxProfit(new int[] { 2, 1, 4, 4, 2, 3, 2, 5, 1, 2 }, 1);
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
 
-            ISolution sln = new PredictTheWinnerSln();
+            ISolution sln = new MaxProfit2Sln();
             sln.Execute();
 
             Console.ReadLine();
