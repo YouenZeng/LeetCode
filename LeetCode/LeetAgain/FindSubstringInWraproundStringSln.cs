4e02be06a632dcaa8982607a72ac97c422bb553b
Bran Zeng <YouEn.Zeng2@adp.com>
2018-03-13 12:03:45 +0800

    WIP

--- LeetCode/LeetAgain/FindSubstringInWraproundStringSln.cs
+++ LeetCode/LeetAgain/FindSubstringInWraproundStringSln.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace LeetCode.LeetAgain
 {
@@ -8,34 +6,12 @@ namespace LeetCode.LeetAgain
     {
         public int FindSubstringInWraproundString(string p)
         {
-
-            int[] result = new int[26];
-            int adjCount = 1;
-            for (int i = 0; i < p.Length; i++)
-            {
-                //adj chars
-                if (i > 0 && ((p[i - 1] == 'z' && p[i] == 'a') || p[i] - p[i - 1] == 1))
-                {
-                    adjCount++;
-                }
-                else
-                {
-                    adjCount = 1;
-                }
-
-                int index = p[i] - 'a';
-                result[index] = Math.Max(result[index], adjCount);
-            }
-            int count = 0;
-            foreach (var item in result)
-            {
-                count += item;
-            }
-            return count;
+            
+            throw new NotImplementedException();
         }
         void ISolution.Execute()
         {
-            FindSubstringInWraproundString("abcdbcd");
+            throw new System.NotImplementedException();
         }
     }
 }
