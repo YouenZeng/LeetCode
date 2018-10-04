a664e86b087cece6de0244b1d2ec579d9854171f
Bran Zeng <YouEn.Zeng2@adp.com>
2018-10-04 22:20:56 +0800

    at home

--- LeetCode/LeetAgain/TopKFrequentSln.cs
+++ LeetCode/LeetAgain/TopKFrequentSln.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 
 namespace LeetCode.LeetAgain
 {
     class TopKFrequentSln : ISolution
     {
+        public IList<int> TopKFrequentHeapSort(int[] nums, int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HeapSort(int[] nums)
+        {
+            int n = nums.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(nums, n, i);
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = nums[0];
+                nums[0] = nums[i];
+                nums[i] = temp;
+
+                Heapify(nums, i, 0);
+            }
+        }
+
+        private void Heapify(int[] nums, int heapSize, int currentRootIndex)
+        {
+            int leftIndex = currentRootIndex * 2;
+            int rightIndex = leftIndex + 1;
+
+            int largest = currentRootIndex;
+            if (leftIndex < heapSize && nums[leftIndex] < nums[currentRootIndex])
+            {
+                largest = leftIndex;
+            }
+
+            if (rightIndex < heapSize && nums[currentRootIndex] < nums[rightIndex])
+            {
+                largest = rightIndex;
+            }
+
+            if (largest != currentRootIndex)
+            {
+                int swap = nums[currentRootIndex];
+                nums[currentRootIndex] = nums[largest];
+                nums[largest] = swap;
+
+                Heapify(nums, heapSize, largest);
+            }
+        }
+
+        /// <summary>
+        /// 桶
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
         public IList<int> TopKFrequent(int[] nums, int k)
         {
             Dictionary<int, int> numsCount = new Dictionary<int, int>();
@@ -31,7 +85,7 @@ namespace LeetCode.LeetAgain
             List<int> result = new List<int>();
             for (int i = bucket.Length - 1; i >= 0 && result.Count < k; i--)
             {
-                if (bucket[i]!=null && bucket[i].Count>0)
+                if (bucket[i] != null && bucket[i].Count > 0)
                 {
                     result.AddRange(bucket[i]);
                 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,7 +10,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new SolveNQueensSln();
+            ISolution sln = new PartitionLabelsSln();
             sln.Execute();
 
             Console.ReadLine();
