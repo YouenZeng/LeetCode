b6686a0c483b76abfdcfcd4135ca27bc176030e4
Bran Zeng <YouEn.Zeng2@adp.com>
2019-02-11 22:51:49 +0800

    hmmm;

--- LeetCode/LeetAgain/ReconstructQueueSln.cs
+++ LeetCode/LeetAgain/ReconstructQueueSln.cs
@@ -7,40 +7,9 @@ namespace LeetCode.LeetAgain
     {
         public int[,] ReconstructQueue(int[,] people)
         {
-            SortedDictionary<int, List<int>> peopleDict = new SortedDictionary<int, List<int>>();
-
-            for (int i = 0; i < people.GetLength(0); i++)
-            {
-                if (peopleDict.ContainsKey(people[i, 0]))
-                {
-                    peopleDict[people[i, 0]].Add(people[i, 1]);
-                }
-                else
-                {
-                    peopleDict.Add(people[i, 0], new List<int>() { people[i, 1] });
-                }
-            }
-
-            SortedSet<int> inQueue = new SortedSet<int>();
-
             int[,] result = new int[people.GetLength(0), 2];
-            foreach (KeyValuePair<int, List<int>> item in peopleDict)
-            {
-                List<int> positionList = item.Value;
-                IOrderedEnumerable<int> pp = positionList.OrderByDescending(p => p);
-                foreach (int position in pp)
-                {
-                    int p = position;
-                    foreach (int q in inQueue)
-                    {
-                        if (p >= q) p++;
-                    }
-                    result[p, 0] = item.Key;
-                    result[p, 1] = position;
-                    inQueue.Add(p);
-                }
-            }
-            return result;
+
+            
         }
         public void Execute()
         {
