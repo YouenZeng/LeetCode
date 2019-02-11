bb8c5d05ac62823f96a461378054335e398caeb8
Bran Zeng <YouEn.Zeng2@adp.com>
2019-02-11 23:29:17 +0800

    inorder traversal with recusive

---
+++ LeetCode/LeetAgain/MinDiffInBSTSln.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Leets;
+using System;
+
+namespace LeetCode.LeetAgain
+{
+    internal class MinDiffInBSTSln : ISolution
+    {
+        private int result = int.MaxValue;
+        private int preNode = -1;
+        public int MinDiffInBST(TreeNode root)
+        {
+            if (root.left != null) MinDiffInBST(root.left);
+            if (preNode >= 0) result = Math.Min(result, root.val - preNode);
+
+            preNode = root.val;
+
+            if (root.right != null) MinDiffInBST(root.right);
+            return result;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
