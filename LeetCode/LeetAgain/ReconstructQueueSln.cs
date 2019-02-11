453657d7c03c3761b7a229a1940e17c48040ea26
Bran Zeng <YouEn.Zeng2@adp.com>
2019-02-11 19:09:59 +0800

    char

---
+++ LeetCode/LeetAgain/ReconstructQueueSln.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class ReconstructQueueSln : ISolution
+    {
+        public int[,] ReconstructQueue(int[,] people)
+        {
+
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode/LeetAgain/ToLowerCaseSLn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class ToLowerCaseSln : ISolution
+    {
+        public string ToLowerCase(string str)
+        {
+            //A 65 Z 90
+            //a 97 Z 122
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 65 && str[i]<=90)
+                {
+                    sb.Append((char)(str[i] + 32));
+                }
+                else
+                {
+                    sb.Append(str[i]);
+                }
+            }
+            return sb.ToString();
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
