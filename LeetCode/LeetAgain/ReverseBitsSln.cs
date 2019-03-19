a5dd71172d3cf04ad9c2b2d1fb1ded087bba7f3c
YouenZeng <raaaazer@gmail.com>
2019-03-19 22:12:23 +0800

    190: bitwise

---
+++ LeetCode/LeetAgain/ReverseBitsSln.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCode.LeetAgain
+{
+    class ReverseBitsSln : ISolution
+    {
+        public uint ReverseBits(uint n)
+        {
+            uint result = 0;
+            uint one = 1;
+            for (int i = 0; i < 32; i++)
+            {
+                if (n % 2 == 1)
+                {
+                    result += (one << (31 - i));
+                }
+                n = n / 2;
+            }
+            return result;
+        }
+        public void Execute()
+        {
+            Debug.Assert(ReverseBits(1) == 964176192);
+        }
+    }
+}
