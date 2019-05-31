9995f4725f404194e95425ea9f89c74d57453de6
Bran Zeng <YouEn.Zeng2@adp.com>
2019-05-31 18:29:09 +0800

    wip

---
+++ LeetCode/LeetAgain/LeastBricksSln.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class LeastBricksSln:ISolution
+    {
+        public int LeastBricks(IList<IList<int>> wall)
+        {
+
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
