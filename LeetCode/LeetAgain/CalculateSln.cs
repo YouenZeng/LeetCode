9966b5a84e613378fbefafa8910df4b3b602796a
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-12 15:19:38 +0800

    stack solution

--- LeetCode/LeetAgain/CalculateSln.cs
+++ LeetCode/LeetAgain/CalculateSln.cs
@@ -7,24 +7,93 @@ namespace LeetCode.LeetAgain
 {
     class CalculateSln : ISolution
     {
+
+        HashSet<char> hs = new HashSet<char>(
+
+            new List<char>()
+            {
+                ' ','(',')','+','-'
+            }
+        );
         public int Calculate(string s)
         {
-            string trimString = s.Replace(" ", "");
-
             Stack<string> cStack = new Stack<string>();
 
-            //1. unify data
-            //2. add to stack
-            //2.1 while met ), find corresponding (, and calculate value
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                    continue;
+                if (s[i] == '+' || s[i] == '-'|| s[i] == '(')
+                {
+                    cStack.Push(s[i].ToString());
+                    continue;
+                }
 
-            //3. calculate result of stack(no parentheses)
+                if (s[i] == ')')
+                {
+                    int r = HandleP(cStack);
+                    cStack.Push(r.ToString());
+                    continue;
+                }
 
-            throw new NotImplementedException();
+                string s1 = string.Empty;
+
+                while (i < s.Length && !hs.Contains(s[i]))
+                {
+                    s1 += s[i];
+                    i++;
+                }
+
+                i--;
+                cStack.Push(s1);
+            }
+
+            return HandleP(cStack);
         }
+
+        private int HandleP(Stack<string> cStack)
+        {
+            Stack<string> op = new Stack<string>();
+            while (cStack.Count > 0)
+            {
+                var c = cStack.Pop();
+                if (c == "(")
+                {
+                    break;
+                }
+
+                op.Push(c);
+            }
+
+            int r = 0;
+            string addOrMinus = "+";
+            while (op.Count > 0)
+            {
+                var c = op.Pop();
+
+                if (c == "+" || c == "-")
+                {
+                    addOrMinus = c;
+                    continue;
+                }
+
+                int num = Convert.ToInt32(c);
+
+                if (addOrMinus == "+")
+                    r += num;
+                else
+                {
+                    r -= num;
+                }
+            }
+
+            return r;
+        }
+
 
         public void Execute()
         {
-            string input = @"2-4-(8+2-6+(8+4-(1)+8-10))";
+            string input = @"2584";
             Calculate(input);
         }
     }
