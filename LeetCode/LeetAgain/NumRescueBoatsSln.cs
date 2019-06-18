6a8c314bac3a638a8e2805ea60bdc39e8f4f381f
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-18 12:26:06 +0800

    greedy solution

--- LeetCode/LeetAgain/NumRescueBoatsSln.cs
+++ LeetCode/LeetAgain/NumRescueBoatsSln.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace LeetCode.LeetAgain
@@ -11,52 +12,40 @@ namespace LeetCode.LeetAgain
             //greedy
             Array.Sort(people);
             int count = 0;
-            Dictionary<int, int> sDict = new Dictionary<int, int>();
 
-            for (int i = 0; i < people.Length; i++)
+            int start = 0;
+            int end = people.Length - 1;
+
+            var target = limit - people[start];
+            while (people[end] > target && start <= end && end > 0)
             {
-                if (sDict.ContainsKey(people[i]))
+                end--;
+            }
+
+            int endIdx = end;
+            while (start <= end)
+            {
+                target = limit - people[start];
+
+                if (people[end] <= target)
                 {
-                    sDict[people[i]] += 1;
+                    start++;
+                    end--;
+                    count++;
                 }
                 else
                 {
-                    sDict.Add(people[i], 1);
+                    count++;
+                    end--;
                 }
             }
 
-            for (int i = 0; i < people.Length; i++)
-            {
-                if (sDict[people[i]] > 0)
-                {
-                    int target = limit - people[i];
-                    bool foundMatch = false;
-                    for (int j = people.Length - 1; j >= 0; j--)
-                    {
-                        if (sDict[people[j]] > 0 && people[j] <= target)
-                        {
-                            foundMatch = true;
-                            count++;
-                            sDict[people[j]] -= 1;
-                            break;
-                        }
-                    }
-
-                    if (!foundMatch)
-                    {
-                        count++;
-                    }
-                }
-
-                sDict[people[i]] -= 1;
-            }
-
-            return count;
+            return count + people.Length - endIdx - 1;
         }
         public void Execute()
         {
-            NumRescueBoats(new[] {1,2,2,3}, 3);
-            NumRescueBoats(new[] {1, 2, 3, 4, 5, 6, 7, 8}, 9);
+            NumRescueBoats(new[] { 1, 2, 2, 3 }, 3);
+            NumRescueBoats(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 9);
             NumRescueBoats(new[] { 11, 12, 13, 14, 15, 16, 17, 18 }, 19);
         }
     }
