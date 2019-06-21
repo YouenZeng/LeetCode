404f3e190b75bbb82c52d38f7c39137787944481
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-21 16:46:49 +0800

    roman to int

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,7 +9,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new IntToRomanSLn();
+            ISolution sln = new RomanToIntSln();
             sln.Execute();
 
             Console.ReadLine();
