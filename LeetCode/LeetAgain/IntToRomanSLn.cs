7beca3fe4b3b2d0623182448415a714cc1399eb7
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-21 16:29:50 +0800

    12

---
+++ LeetCode/LeetAgain/IntToRomanSLn.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class IntToRomanSLn : ISolution
+    {
+        public string IntToRoman(int num)
+        {
+            string result = string.Empty;
+
+            var l = new List<int>()
+            {
+                1000,
+                900,
+                500,
+                400,
+                100,
+                90,
+                50,
+                40,
+                10,
+                9,
+                5,
+                4,
+                1,
+            };
+            var l2 = new List<string>()
+            {
+                "M",
+                "CM",
+                "D",
+                "CD",
+                "C",
+                "XC",
+                "L",
+                "XL",
+                "X",
+                "IX",
+                "V",
+                "IV",
+                "I",
+            };
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                if (l2[i].Length > 1)
+                {
+                    if (num / l[i] > 0)
+                    {
+                        num -= l[i];
+                        result += l2[i];
+                    }
+                }
+                else
+                {
+                    while (num >= l[i])
+                    {
+                        num -= l[i];
+                        result += l2[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void Execute()
+        {
+            IntToRoman(180);
+            IntToRoman(4);
+            IntToRoman(9);
+            IntToRoman(10);
+            IntToRoman(189);
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,7 +9,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new FindMedianSortedArraysSln();
+            ISolution sln = new IntToRomanSLn();
             sln.Execute();
 
             Console.ReadLine();
