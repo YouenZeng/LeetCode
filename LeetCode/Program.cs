0749eda79180ff5a4ab9663df6d1e5fef0ed3957
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-26 16:48:40 +0800

    DFS

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new ThreeSumClosestSln();
+            ISolution sln = new LetterCombinationsSln();
             sln.Execute();
 
             Console.ReadLine();
