1816bbf8e5b98cc544ffb084c7de409a2dcb2094
Bran Zeng <YouEn.Zeng2@adp.com>
2019-06-26 17:39:36 +0800

    4 sum WIP

---
+++ LeetCode/LeetAgain/FourSumSln.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LeetAgain
+{
+    class FourSumSln:ISolution
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
