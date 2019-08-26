e3b8fadb69c82fae6956fbb6cf43f6048903eba8
Bran Zeng <YouEn.Zeng2@adp.com>
2019-08-26 17:52:22 +0800

    The bad!

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new LengthOfLastWordSln();
+            ISolution sln = new GenerateMatrixSln();
             sln.Execute();
 
             Console.ReadLine();
