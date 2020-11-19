3b93ea8f00decd079f881e6523932533085cac7d
Bran Zeng <YouEn.Zeng2@adp.com>
2020-11-19 18:24:25 +0800

    WIP

---
+++ LeetCode/Challenge/DecodeStringSln.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Challenge
+{
+    class DecodeStringSln : ISolution
+    {
+        public string DecodeString(string s)
+        {
+            List<string> temp = new List<string>();
+
+            int start = 0;
+            int end = s.Length;
+
+            Stack<char> stack = new Stack<char>(s.ToCharArray());
+            while (stack.Count > 0)
+            {
+                var p = stack.Pop();
+                if (p >= '0' && p <= '9')
+                {
+                }
+            }
+
+
+            return string.Join("", temp);
+        }
+
+
+        public void Execute()
+        {
+            string input = "9[9[9[a]b9[a]]]";
+            Console.WriteLine(DecodeString(input));
+        }
+    }
+}
