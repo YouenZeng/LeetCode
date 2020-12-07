fe13bfa7fd6194d7d34d4c0f65eee61fe4741a95
Bran Zeng <YouEn.Zeng2@adp.com>
2020-12-07 10:39:37 +0800

    .

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,7 +10,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ISolution sln = new LongestPrefixSln();
+            ISolution sln = new LeetAgain.LongestPrefixSln();
             sln.Execute();
 
 
